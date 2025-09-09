58cb7028f04b07c1fa1b1a54f55b84504c95c1bf
RajChandan <raj.chandan91@gmail.com>
2025-09-10 00:11:31 +0530

    service new lists

--- service/urls.py
+++ service/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('webinars/', views.webinars, name='webinars'),
-    path('workshops/', views.workshops, name='workshops'),
-    path('courses/', views.courses, name='courses'),
-    path('dfy-tools/', views.dfy_tools, name='dfy_tools'),
-    path('retainer/', views.retainer, name='retainer'),
-    path('accelerator/', views.accelerator, name='accelerator'),
+    path("webinars/", views.webinars, name="webinars"),
+    path("workshops/", views.workshops, name="workshops"),
+    path("courses/", views.courses, name="courses"),
+    path("dfy-tools/", views.dfy_tools, name="dfy_tools"),
+    path("retainer/", views.retainer, name="retainer"),
+    path("accelerator/", views.accelerator, name="accelerator"),
+    path("consulting-strategy/", views.consulting_strategy, name="consulting_strategy"),
+    path("AI-integration/", views.AI_integration, name="AI_integration"),
+    path("skills-training/", views.skills_training, name="skills_training"),
 ]
--- service/views.py
+++ service/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 
+
 def webinars(request):
-    return render(request, 'service/webinars.html')
+    return render(request, "service/webinars.html")
+
 
 def workshops(request):
-    return render(request, 'service/workshops.html')
+    return render(request, "service/workshops.html")
+
 
 def courses(request):
-    return render(request, 'service/courses.html')
+    return render(request, "service/courses.html")
+
 
 def dfy_tools(request):
-    return render(request, 'service/dfy_tools.html')
+    return render(request, "service/dfy_tools.html")
+
 
 def retainer(request):
-    return render(request, 'service/retainer.html')
+    return render(request, "service/retainer.html")
+
 
 def accelerator(request):
-    return render(request, 'service/accelerator.html')
+    return render(request, "service/accelerator.html")
+
+
+def consulting_strategy(request):
+    return render(request, "service/consulting_strategy.html")
+
+
+def AI_integration(request):
+    return render(request, "service/AI_integration.html")
+
+
+def skills_training(request):
+    return render(request, "service/skills_training.html")
